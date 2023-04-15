71753e5d1953dbe1bccfc07f8126ab461fd77647
p3zz <federico.pezzato.work@gmail.com>
2023-04-15 18:06:55 +0200

    add dynamic map with seqn

--- analyze-map.py
+++ analyze-map.py
@@ -2,15 +2,35 @@ import graphics as g
 import csv
 import sys
 import os
+import time
 
-class Node:
-    def __init__(self, id, x, y):
-        self.id = id
+class Position:
+    def __init__(self, x, y):
         self.x = x
         self.y = y
     
     def __str__(self):
-        return "Id: {}\tx: {}\ty: {}".format(self.id, self.x, self.y)
+        return "x: {}\ty: {}".format(self.x, self.y)
+
+class CollectionData:
+    def __init__(self, seqn, parent):
+        self.seqn = seqn
+        self.parent = parent
+    
+    def __str__(self):
+        return "seqn: {}\tparent: {}".format(self.seqn, self.parent)
+
+class Node:
+    def __init__(self, id, position, collection_data):
+        self.id = id
+        self.position = position
+        self.collection_data = collection_data
+    
+    def __str__(self):
+        result = "Id: {}\tPosition: {}\n".format(self.id, self.position)
+        for data in self.collection_data:
+            result += "{}\n".format(data)
+        return result
 
 class ResultType:
     UDGM = 0
@@ -23,35 +43,72 @@ def parse_map(filename, nodes):
             node_id = int(row["id"])
             x = float(row["x"])
             y = float(row["y"])
-            nodes[node_id] = Node(node_id, x, y)
+            nodes[node_id] = Node(node_id, Position(x,y), [])
+
+def parse_collection(filename, nodes):
+    with open(filename, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter='\t')
+        for row in csv_reader:
+            node_id = int(row["src"])
+            parent = int(row["parent"])
+            seqn = int(row["seqn"])
+            nodes[node_id].collection_data.append(CollectionData(seqn, parent))
+            # print(nodes[node_id])
+
+def draw_link(win, src, dst):
+    c = g.Line(g.Point(src.position.x, src.position.y), g.Point(dst.position.x, dst.position.y))
+    c.draw(win)
 
 def draw_node(win, node):
-    c = g.Circle(g.Point(node.x, node.y), 8)
-    t = g.Text(g.Point(node.x, node.y), str(node.id))
+    c = g.Circle(g.Point(node.position.x, node.position.y), 8)
+    t = g.Text(g.Point(node.position.x, node.position.y), str(node.id))
     t.setSize(8)
     t.setFill("black")
     t.draw(win)
     c.draw(win)
 
+def draw_seqn(win, seqn, x, y):
+    t = g.Text(g.Point(x, y), "Seqn: {}".format(seqn))
+    t.setSize(20)
+    t.setFill("black")
+    t.draw(win)
+
+def clear(win):
+    for item in win.items[:]:
+        item.undraw()
+    win.update()
+
 def draw_map(nodes, result_type: ResultType):
     scale = 5 if result_type == ResultType.UDGM else 0.5
     offset = 20
+    for key in nodes:
+        x_scaled = (nodes[key].position.x * scale) + offset
+        y_scaled = (nodes[key].position.y * scale) + offset
+        nodes[key] = Node(nodes[key].id, Position(x_scaled, y_scaled), nodes[key].collection_data) 
+        
     nds = list(nodes.values())
-    nds_scaled = list(map(lambda node: Node(node.id, (node.x * scale) + offset, (node.y * scale) + offset), nds))
-    height = max(list(map(lambda node: node.y, nds_scaled)))
-    width = max(list(map(lambda node: node.x, nds_scaled)))
-    win = g.GraphWin("nodes map", width + offset, height + offset)
-    for node in nds_scaled:
-        draw_node(win, node)
-    keyString = ""
-    while keyString!="q":
-        keyString = win.getKey() # Pause to view result
+    height = max(list(map(lambda node: node.position.y, nds)))
+    width = max(list(map(lambda node: node.position.x, nds)))
+    win = g.GraphWin("nodes map", width + offset, height + offset, autoflush=True)
+    index = 0
+    while True:
+        for node in nds:
+            draw_node(win, node)
+            collection = list(filter(lambda d: d.seqn == index, node.collection_data))
+            for data in collection:
+                draw_link(win, node, nodes[data.parent])
+        draw_seqn(win, index, width * 0.5, offset)
+        index+=1
+        time.sleep(0.5)
+        clear(win)
+        
     win.close()    # Close window when done
 
 if __name__ == '__main__':
     args = sys.argv
     test_path = args[1]
     map_filename = "map.csv"
+    recv_filename = "test-recv.csv"
     udgm_dirname = "UDGM"
     mrm_dirname = "MRM"
 
@@ -61,7 +118,14 @@ if __name__ == '__main__':
     udgm_map_path = os.path.join(test_path, udgm_dirname, map_filename)
     mrm_map_path = os.path.join(test_path, mrm_dirname, map_filename)
 
+    udgm_recv_path = os.path.join(test_path, udgm_dirname, recv_filename)
+    mrm_recv_path = os.path.join(test_path, mrm_dirname, recv_filename)
+
     parse_map(udgm_map_path, udgm_nodes)
     parse_map(mrm_map_path, mrm_nodes)
 
-    draw_map(udgm_nodes, ResultType.UDGM)
+    parse_collection(udgm_recv_path, udgm_nodes)
+    parse_collection(mrm_recv_path, mrm_nodes)
+
+    # print(udgm_nodes)
+    draw_map(mrm_nodes, ResultType.MRM)
