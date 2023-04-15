6665f13cfb696f677ca9e5e9f7a342517103bb3f
p3zz <federico.pezzato.work@gmail.com>
2023-04-15 13:18:24 +0200

    update simulate.sh with map creation

--- simulate.sh
+++ simulate.sh
@@ -20,25 +20,39 @@ mkdir ${TEST_DIR} && \
 # create configuration file
 python3 create-config.py > ${TEST_DIR}/config.txt &&
 
-# run UDGM Simulation
+# create UDGM folder
 mkdir ${TEST_UDGM_DIR} &&
+
+# create UDGM map file
+python3 create-map.py test_nogui.csc > ${TEST_UDGM_DIR}/map.csv &&
+
+# run UDGM simulation
 echo UDGM Simulation started && \
 cooja_nogui ${ROOT_DIR}/test_nogui.csc
 
 wait
 
 echo UDGM Simulation terminated && \
+
+# parse UDGM stats
 mv ${ROOT_DIR}/test*.log ${TEST_UDGM_DIR} &&
 python3 ${ROOT_DIR}/parse-stats.py ${TEST_UDGM_DIR}/test.log > ${TEST_UDGM_DIR}/result.txt
 
-# run MRM Simulation
+# create MRM folder
 mkdir ${TEST_MRM_DIR} &&
+
+# create MRM map file
+python3 create-map.py test_nogui_mrm.csc > ${TEST_MRM_DIR}/map.csv &&
+
+# run MRM simulation
 echo MRM Simulation started && \
 cooja_nogui ${ROOT_DIR}/test_nogui_mrm.csc
 
 wait
 
 echo MRM Simulation terminated && \
+
+# parse MRM stats
 mv ${ROOT_DIR}/test*.log ${TEST_MRM_DIR} &&
 python3 ${ROOT_DIR}/parse-stats.py ${TEST_MRM_DIR}/test.log > ${TEST_MRM_DIR}/result.txt
 
