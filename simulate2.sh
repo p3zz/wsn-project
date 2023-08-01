257623c18b195c4ce1b9224cf3fad41f974d6b9f
p3zz <federico.pezzato.work@gmail.com>
2023-08-01 15:51:19 +0200

    fix simulate2

--- simulate2.sh
+++ simulate2.sh
@@ -7,10 +7,10 @@ DEST_DIR=$2
 make &&
 
 # create csv of the map
-python3 $(pwd)/create-map.py ${COOJA_CONFIG_PATH} > ${DEST_DIR}/map.csv &&
+python3 $(pwd)/tools/create-map.py ${COOJA_CONFIG_PATH} > ${DEST_DIR}/map.csv &&
 
 # create config
-python3 $(pwd)/create-config.py ${COOJA_CONFIG_PATH} > ${DEST_DIR}/config.txt &&
+python3 $(pwd)/tools/create-config.py ${COOJA_CONFIG_PATH} > ${DEST_DIR}/config.txt &&
 
 # run simulation
 echo Simulation started && \
@@ -23,4 +23,4 @@ echo Simulation terminated &&
 # parse stats
 mv $(pwd)/test*.log ${DEST_DIR} &&
 echo Parsing stats &&
-python3 $(pwd)/parse-stats.py ${DEST_DIR}/test.log > ${DEST_DIR}/result.txt
+python3 $(pwd)/tools/parse-stats.py ${DEST_DIR}/test.log > ${DEST_DIR}/result.txt
